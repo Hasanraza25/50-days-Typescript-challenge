fca979f99baf7a1eed83b4c3eef13a6e6d4015c3
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-10 02:02:03 +0500

    Add day 9

---
+++ day 9/ques25.ts
@@ -0,0 +1,11 @@
+// Version that passes
+let alien_color = "green";
+
+if(alien_color === "green"){
+    console.log("The player just earned 5 points.");
+}
+// Version that fails
+alien_color = "yellow";
+if(alien_color === "green"){
+    console.log("The player just earned 5 points.");
+}
---
+++ day 9/ques26.ts
@@ -0,0 +1,18 @@
+// Runs if block
+let alien_color = "green";
+
+if(alien_color === "green"){
+    console.log("The player just earned 5 points.");
+}
+else{
+    console.log("The player just earned 10 points.");
+}
+
+// Runs else block
+alien_color = "yellow";
+if(alien_color === "green"){
+    console.log("The player just earned 5 points.");
+}
+else{
+    console.log("The player just earned 10 points.");
+}
