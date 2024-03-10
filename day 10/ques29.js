c00909b4d9efe38274503a80bf0f5bfb55f89332
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-11 02:02:23 +0500

    Add day 10

---
+++ day 10/ques29.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ["Mango", "Apple", "Banana"];
+if (favorite_fruits.indexOf("Mango") !== -1) {
+    console.log("I really like Mango.");
+}
+if (favorite_fruits.indexOf("Apple") !== -1) {
+    console.log("I really like Apple.");
+}
+if (favorite_fruits.indexOf("Banana") !== -1) {
+    console.log("I really like Banana.");
+}
+if (favorite_fruits.indexOf("Oranges") !== -1) {
+    console.log("I really like Oranges.");
+}
+if (favorite_fruits.indexOf("Strawberries") !== -1) {
+    console.log("I really like Strawberries.");
+}
