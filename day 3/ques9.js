e2abd8e515c6f14e15b466a84330c08d7081d13e
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:38:26 +0500

    Delete day 3 directory

--- day 3/ques9.js
+++
@@ -1,3 +0,0 @@
-//  Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
-var favNum = 6;
-console.log("My favorite number is ".concat(favNum, "."));
