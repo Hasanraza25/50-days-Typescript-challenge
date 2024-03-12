81346395079ab4f0cb458e0e585333953c66a063
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-12 05:56:32 +0500

    Add day 11

---
+++ day 11/ques32.js
@@ -0,0 +1,15 @@
+var current_users = ["Hasan", "Hussain", "Haram", "Hasnain", "Ali"];
+var new_users = ["Ahmed", "Shakeel", "Hussain", "Jawed", "Haram"];
+var _loop_1 = function (new_user) {
+    var lowercase_new_user = new_user.toLowerCase();
+    if (current_users.some(function (user) { return user.toLowerCase() === lowercase_new_user; })) {
+        console.log("".concat(new_user, " will need to enter a new username"));
+    }
+    else {
+        console.log("".concat(new_user, " is available"));
+    }
+};
+for (var _i = 0, new_users_1 = new_users; _i < new_users_1.length; _i++) {
+    var new_user = new_users_1[_i];
+    _loop_1(new_user);
+}
