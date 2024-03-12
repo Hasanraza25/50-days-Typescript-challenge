89668a0ae1554f0f68d5985a55fd1b65a0d2610f
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:38:15 +0500

    Delete day 2 directory

--- day 2/ques6.ts
+++
@@ -1,5 +0,0 @@
-// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
-let perName: string = "\t\n Hussain Raza \t\n";
-
-console.log(perName);
-console.log(perName.trim());
