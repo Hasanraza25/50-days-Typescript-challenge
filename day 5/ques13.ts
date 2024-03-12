88fe072e4e62ff04ef6a04f160a66ddc91b25c2b
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:39:07 +0500

    Delete day 5 directory

--- day 5/ques13.ts
+++
@@ -1,6 +0,0 @@
-// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-
-let favTransports: string[] = ["Audi e-tron GT","Toyota Corolla", "Honda CG 125", "Yamaha YZF-R1", "Vespa Primavera" ];
-favTransports.forEach(mode => {
-    console.log(`I would like to own a ${mode}.`);
-})
--- day 5/ques14.js
+++
@@ -1,5 +0,0 @@
-// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-var guests = ["Jawed", "Osman", "Shakeel", "Saeed", "Sattar"];
-guests.forEach(function (guest) {
-    console.log("Dear ".concat(guest, ", You're invited to dinner!"));
-});
