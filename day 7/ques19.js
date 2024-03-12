75d484032c899f0ea41c4f068139afa3e5936c4f
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:39:24 +0500

    Delete day 7 directory

--- day 7/ques19.js
+++
@@ -1,3 +0,0 @@
-// Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
-var guests = ["Hussain", "Jawed", "Osman", "Haram", "Asif", "Saeed", "Sattar", "Hasnain"];
-console.log("I'm inviting ".concat(guests.length, " people to dinner."));
--- day 7/ques20.js
+++
@@ -1,5 +0,0 @@
-//  Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
-var programmingLang = ["TypeScript", "JavaScript", "Python", "Java", "C++", "PHP"];
-programmingLang.forEach(function (language) {
-    console.log(language);
-});
