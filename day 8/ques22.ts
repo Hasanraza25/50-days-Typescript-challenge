21e9f9d77bb63dfb2611a705c01621080cf045ac
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:39:33 +0500

    Delete day 8 directory

--- day 8/ques22.ts
+++
@@ -1,8 +0,0 @@
-//  Intentional Error
-
-let favPlaces: string[] = ["Madina Munawara", "Kashmir", "Skardu", "London", "Paris"];
-// Intentional error
-console.log(favPlaces[5]);
-// Correct the error
-console.log(favPlaces[4]);
-
