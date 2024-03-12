216dc3427938509e7ec0832bbced81085d4e150d
Hasan Raza <160281891+Hasanraza09@users.noreply.github.com>
2024-03-13 03:38:58 +0500

    Delete day 4 directory

--- day 4/ques11.ts
+++
@@ -1,7 +0,0 @@
-// Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
-
-let myFriends: string[] = ["Shakir", "Kareem", "Hanzala", "Haris", "Hasnain"];
-
-for(let i=0; i < myFriends.length; i++){
-    console.log(myFriends[i]);
-}
--- day 4/ques12.ts
+++
@@ -1,8 +0,0 @@
-// Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
-
-let myFriends: string[] = ["Shakir", "Kareem", "Hanzala", "Haris", "Hasnain"];
-
-for(let name of myFriends){
-    console.log(`Hello ${name}, How are you?`);
-}
-    
