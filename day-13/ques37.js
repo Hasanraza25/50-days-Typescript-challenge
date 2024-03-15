dbad0bc5fba4220339113955fea5a2aa6ae12d9d
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-16 03:08:01 +0500

    update day-13

--- day-13/ques37.js
+++ day-13/ques37.js
@@ -1,7 +1,7 @@
 function make_shirt(size, message) {
     if (size === void 0) { size = "large"; }
     if (message === void 0) { message = "I love TypeScript"; }
-    console.log("The size of shirt is ".concat(size, " and the message printed on it is ").concat(message, "."));
+    console.log("The size of shirt is ".concat(size, " and the message printed on it is \"").concat(message, "\"."));
 }
 // For Large Shirt
 make_shirt();
