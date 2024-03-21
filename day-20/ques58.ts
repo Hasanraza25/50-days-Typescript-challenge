9cda0219c260514c18a9517e99185675c993f4ef
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-21 17:29:00 +0500

    add day-20

--- day-20/ques58.ts
+++ day-20/ques58.ts
@@ -4,6 +4,7 @@ function averageCalculator(...scores: number[]) {
     let average = scores.reduce((sum,score) => sum + score, 0)/ scores.length;
     console.log(average);
 }
+
 // 0 is our initial value : 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 
 averageCalculator(1,2,3,4,5,6,7,8,9); // Output : 5
