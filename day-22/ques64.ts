6ecde71895fa0d2933859da4de760a99d46f43dc
Hasanraza09 <hasanhussain2580@gmail.com>
2024-03-25 03:36:20 +0500

    day-22

---
+++ day-22/ques64.ts
@@ -0,0 +1,9 @@
+// Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
+
+function joinStringAndNum(text: string, number: number) {
+    return text + number;
+}
+
+console.log(joinStringAndNum("Age: ", 18));
+
+// Output: Age: 18
---
+++ day-22/ques66.ts
@@ -0,0 +1,9 @@
+// Logical AND Verification: Create a function that checks two boolean (true or false) values. It should only say true if both are true, using the && operator. For instance, checkBothTrue(true, false) should be false.
+
+function checkBoolean(val1: boolean, val2: boolean): boolean {
+    return val1 && val2;
+}
+
+console.log(checkBoolean(true, false));
+
+// Output: false
