ec9718a141f845fb1a4bb819b6be6944ae790892
Hasan Raza <hasanhussain2580@gmail.com>
2024-04-10 20:32:33 +0500

    add day-40

---
+++ day-40/day-40.ts
@@ -0,0 +1,20 @@
+// Write a loop that logs numbers from 1 to 10 to the console.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Create a while loop that logs "Hello, World!" 5 times.
+
+let count: number = 0
+while(count< 5){
+    console.log("Hello World");
+    count++;
+}
+
+// Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+let favMovies: string[] = ['Avengers', '12th fail', '3 idiots', 'Taare zameen par', 'Dunky']
+for(let val of favMovies){
+    console.log(val);
+}
