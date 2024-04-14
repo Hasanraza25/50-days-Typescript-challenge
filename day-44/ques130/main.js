bf4c2e1624de7505ac9344802784f4f557a2674a
Hasan Raza <hasanhussain2580@gmail.com>
2024-04-15 00:05:51 +0500

    update: day-44

--- day-44/ques130/main.js
+++ day-44/ques130/main.js
@@ -2,5 +2,5 @@
 // Explain how to export a function from one JavaScript file and import it into another file.
 Object.defineProperty(exports, "__esModule", { value: true });
 // Import function 
-var function_1 = require("./function");
-console.log((0, function_1.addNumbers)(12, 8)); // Output: 20
+var function_js_1 = require("./function.js");
+console.log((0, function_js_1.addNumbers)(12, 8)); // Output: 20
--- day-44/ques131/main.js
+++ day-44/ques131/main.js
@@ -2,6 +2,7 @@
 // Create two modules; one that exports a class, and another that imports the class and creates an instance.
 Object.defineProperty(exports, "__esModule", { value: true });
 // Import class
-var person_1 = require("./person");
-var person = new person_1.Person('Hasan', 18);
+var person_js_1 = require("./person.js");
+var person = new person_js_1.Person('Hasan', 18);
 person.greet();
+// Output: Hello, my name is Hasan and I am 18 years old.
