444e6d0e11fb6283399229f5c82b1d1989c30765
Hasan Raza <hasanhussain2580@gmail.com>
2024-05-20 13:51:46 +0500

    add day-64

---
+++ day-64/index.js
@@ -0,0 +1,21 @@
+// Object Definition
+var personExOne;
+personExOne = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+// Object Initialization with type annotation
+var personExTwo = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
